90b79202ff169e6ce508ddce115715a7d483aa67
Eugene Stahov <eugene.stahov@gmail.com>
2023-12-12 00:54:06 +0500

    touchdog.sh: temporary handle backlight control

--- touchdog/touchdog.sh
+++ touchdog/touchdog.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 while sleep 1 ; do 
   cat /sys/class/spi_master/spi0/spi0.0/hwmon/*/temp0 >/dev/null 
+  DISPLAY=:0 xset -q | grep "Monitor is On" && echo 0 | sudo tee /sys/class/backlight/fb_st7796s/bl_power
+  DISPLAY=:0 xset -q | grep "Monitor is in Suspend" && echo 1 | sudo tee /sys/class/backlight/fb_st7796s/bl_power  
 done
