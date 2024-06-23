7095e8890406953f9ea3b29379904712568edec1
Eugene Stahov <eugene.stahov@gmail.com>
2024-06-23 13:35:04 +0500

    stop touchdog.sh spaming to logs
    1. Stop unnecessary invoking sudo (script is already running as root)
    2. Remove unnecessary writing to stdout by tee, script may write into /sys directly still running as root

--- touchdog/touchdog.sh
+++ touchdog/touchdog.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+if [ "$EUID" -ne 0 ]; then echo "Please run as root." >&2; exit 1; fi
+
 while sleep 1 ; do 
   cat /sys/class/spi_master/spi0/spi0.0/hwmon/*/temp0 >/dev/null 
-  DISPLAY=:0 xset -q | grep "Monitor is On" && echo 0 | sudo tee /sys/class/backlight/fb_st7796s/bl_power
-  DISPLAY=:0 xset -q | grep "Monitor is in Suspend" && echo 1 | sudo tee /sys/class/backlight/fb_st7796s/bl_power  
+  DISPLAY=:0 xset -q | grep -q "Monitor is On" && echo 0 > /sys/class/backlight/fb_st7796s/bl_power
+  DISPLAY=:0 xset -q | grep -q "Monitor is in Suspend" && echo 1 > /sys/class/backlight/fb_st7796s/bl_power  
 done
